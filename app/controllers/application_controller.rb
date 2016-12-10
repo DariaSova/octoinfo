f1b0b3e797cd8e210ac6cfa6ba88bbd48a489a0f
Daria Sova <todariasova@gmail.com>
2016-12-09 20:52:59 -0800

    Add basic pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
 
-  def home
-    render html: "Hello, World! Welcome!"
-  end
 end
