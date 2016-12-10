24973cec2be3486b0b441cdeb8da3dd3445a6073
Daria Sova <todariasova@gmail.com>
2016-12-09 22:11:53 -0800

    Add helper authentication functions to application controller
    do not allow access to any pages except home if a user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
+  def authenticate
+    redirect_to root_path unless logged_in?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_filter :authenticate, except: [:home]
 
   def home
     render template: 'home'
