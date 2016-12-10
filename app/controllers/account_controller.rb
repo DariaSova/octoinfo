ea65261fff5af1dfbb429cb184d7bd1731bee1a9
Daria Sova <todariasova@gmail.com>
2016-12-10 15:47:44 -0800

    Use octokit to retrive user's data

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,27 +1,24 @@
 class AccountController < ApplicationController
-  before_filter :authenticate
+  before_filter :authenticate, :set_github
 
   def show
     @account = get_public_information
+    binding.pry
   end
 
   private 
+  def set_github
+    @user = Octokit::Client.new(access_token: session[:session_token]).user
+  end
+
   def get_public_information
-    account = { user: user_information, urls: user_urls, avatar: avatar_url, profile_link: profile_link }
+    account = { user: user_information, avatar: @user.avatar_url, profile_link: @user.html_url }
   end
 
   def user_information
     unwanted = [ 'id', 'type', 'site_admin', 'updated_at' ]
+    res = @user.map { | key, value | [ key, value ] unless key.to_s.include? 'url' or key.to_s.in? unwanted }
 
-    res = params[:account].map { | key, value | [ key, value ] unless key.include? 'url' or key.in? unwanted }
     res.compact.to_h
-  end
-
-  def avatar_url
-    params[:account][:avatar_url]
-  end
-
-  def profile_link
-    params[:account][:html_url]
   end
 end
