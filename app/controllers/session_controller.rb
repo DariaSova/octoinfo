3cd4dd1781cf9d9cbae7048c1ea47ff26f9378e4
Daria Sova <todariasova@gmail.com>
2016-12-10 14:54:42 -0800

    Get session token

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,19 +4,29 @@ class SessionController < ApplicationController
   def create
     user = User.find_or_create_by(provider: auth_hash[:provider], uid: auth_hash[:uid], name: auth_user_name)
     session[:user_id] = user.id unless !user.save
-    hash =  auth_hash["extra"]["raw_info"] }
+    session[:session_token] = session_token
+    hash = user_info
 
     redirect_to controller: 'account', action: 'show', account: hash
   end
 
   def destroy
     session[:user_id] = nil
+    session[:session_token] = nil
     redirect_to root_path
   end
 
   private
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def session_token
+    auth_hash['credentials']['token']
+  end
+
+  def user_info
+    auth_hash["extra"]["raw_info"]
   end
 
   def auth_user_name
