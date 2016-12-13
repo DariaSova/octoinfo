5ae4afa618f7ef75ab5b4dd85670592db3985801
Daria Sova <todariasova@gmail.com>
2016-12-13 12:21:05 -0800

    One more test for account controller
    make sure user has to log in first

--- spec/controller/account_controller_spec.rb
+++ spec/controller/account_controller_spec.rb
@@ -2,31 +2,41 @@ require 'rails_helper'
 
 RSpec.describe AccountController, type: :controller do
   describe 'GET #show' do
-    let(:user) { FactoryGirl.create :user }
 
-    before(:each) do
-      session[:user_id] = user.id
+    context 'when user is logged in' do
+      let(:user) { FactoryGirl.create :user }
+
+      before(:each) do
+        session[:user_id] = user.id
+      end
+
+      it 'should return user information hash' do
+        hash = { login: 'WinniePooh', name: 'WinnieThePooh' }
+        Octokit::Client.any_instance.stub(:user).and_return(hash)
+
+        controller.stub(:user_avatar).and_return('www.github.com/avatars/123')
+        controller.stub(:account_link).and_return('www.github.com/winnie')
+        res_hash = { user: hash, avatar: "www.github.com/avatars/123", profile_link: "www.github.com/winnie" }
+        get :show
+        expect(assigns(:account)).to eq res_hash
+      end
+
+      it 'should render show page' do
+        hash = { login: 'WinniePooh', name: 'WinnieThePooh' }
+        Octokit::Client.any_instance.stub(:user).and_return(hash)
+
+        controller.stub(:user_avatar).and_return('www.github.com/avatars/123')
+        controller.stub(:account_link).and_return('www.github.com/winnie')
+        get :show
+        response.should render_template :show 
+      end
     end
 
-    it 'should return user information hash' do
-      hash = { login: 'WinniePooh', name: 'WinnieThePooh' }
-      Octokit::Client.any_instance.stub(:user).and_return(hash)
-
-      controller.stub(:user_avatar).and_return('www.github.com/avatars/123')
-      controller.stub(:account_link).and_return('www.github.com/winnie')
-      res_hash = { user: hash, avatar: "www.github.com/avatars/123", profile_link: "www.github.com/winnie" }
-      get :show
-      expect(assigns(:account)).to eq res_hash
-    end
-
-    it 'should render show page' do
-      hash = { login: 'WinniePooh', name: 'WinnieThePooh' }
-      Octokit::Client.any_instance.stub(:user).and_return(hash)
-
-      controller.stub(:user_avatar).and_return('www.github.com/avatars/123')
-      controller.stub(:account_link).and_return('www.github.com/winnie')
-      get :show
-      response.should render_template :show 
+    context 'when user is not logged in' do
+      it 'should redirect to the root' do
+        get :show
+        response.should redirect_to root_url
+      end
     end
   end
 end
